315cf2bf7d797e52044f46eb9a1ceb9b68db357a
Kimberly Anderson <kimanderson05@fullsail.edu>
2014-06-01 00:37:20 -0400

    Changing Titles

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title> <?php echo gethostname(); ?> - Kimberly Anderson</title>
+    <title> <?php echo gethostname(); ?> - KA - Home</title>
     <link rel="stylesheet" href="css/foundation.css" />
     <script src="js/vendor/modernizr.js"></script>
   </head>
@@ -27,52 +27,7 @@
       	<div class="panel">
 	        <h3>Thank you for visiting my page.</h3>
             <p>Feel free to browse the links to view my resume and work examples.  There is also a "contact me" link which will allow you to send me a text if you have any additional questions.</p>
-	        <!--<div class="row">
-                <div class="large-12 columns">
-                    <div class="large-4 medium-4 columns">
-                        <p><a href="img/AAAE-Icon.jpg"><img src="img/AAAE-Icon-sm.jpg"/></a><br />Icon designed by me.  Background designed by a co-worker.</p>
-                    </div>
-                    <div class="large-4 medium-4 columns">
-                        <p><a href="img/AAAE-home-screen.jpg"><img src="img/AAAE-home-screen-sm.jpg"/></a><br/>Airplane flies across screen.  Speech bubble scrolls (using jQuery).</p>
-                    </div>
-                    <div class="large-4 medium-4 columns">
-                        <p><a href="img/AAAE-timeline-screen.jpg"><img src="img/AAAE-timeline-screen-sm.jpg"/></a><br/>Timeline of images/information depicting various events in aviation.</p>
-                    </div>
-                </div>
-                <div class="large-12 columns">
-                    <div class="large-4 medium-4 columns">
-                        <p><a href="img/AAAE-video-screen.jpg"><img src="img/AAAE-video-screen-sm.jpg"/></a><br/>Videos highlighting various projects completed by the company for which I work.</p>
-                    </div>
-                    <div class="large-4 medium-4 columns">
-                        <p><a href="img/event-poster.jpg"><img src="img/event-poster-sm.jpg"/></a><br />Poster designed for History of Visual Communications class.</p>
-                    </div>
-                    <div class="large-4 medium-4 columns">
-                        <p><a href="img/music-visualization.jpg"><img src="img/music-visualization-sm.jpg"/></a><br/>Music visualization project for History of Visual Communications class.</p>
-                    </div>
-                </div>
-                <div class="large-12 columns">
-                    <div class="large-4 medium-4 columns">
-                        <p><a href="img/lsc-tshirt.jpg"><img src="img/lsc-tshirt-sm.jpg"/></a><br/>T-shirt designed for Little Star Center... a school for children with Autism.</p>
-                    </div>
-                    <div class="large-4 medium-4 columns">
-                        <p><a href="img/fire-ice.jpg"><img src="img/fire-ice-sm.jpg"/></a><br/>Invitation designed for Fire &amp; Ice Benefit for Little Star Center.</p>
-                    </div>
-                    <div class="large-4 medium-4 columns">
-                        <p><a href="img/lsc-drink-card.jpg"><img src="img/lsc-drink-card-sm.jpg"/></a><br/>Drink flyer for Fire &amp; Ice Benefit for Little Star Center.</p>
-                    </div>
-                </div>
-                <div class="large-12 columns">
-                    <div class="large-4 medium-4 columns">
-                        <p><a href="img/lsc-magnet.jpg"><img src="img/lsc-magnet-sm.jpg"/></a><br/>Car magnet designed for Little Star Center.</p>
-                    </div>
-                    <div class="large-8 columns">
 
-                    </div>
-                </div>
-			</div>
-      	</div>
-      </div>
-    </div>-->
 
     <!--<div class="row">
       <div class="large-8 medium-8 columns">
