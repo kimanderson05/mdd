c038d868d44ff22a4794b1cd37ebab521e5641d9
Kimberly Anderson <kimanderson05@fullsail.edu>
2014-06-01 18:47:53 -0400

    Added Contact page

--- contact.php
+++ contact.php
@@ -26,13 +26,6 @@
     <div class="large-12 columns">
         <div class="panel">
             <h3>If you have any questions about my work, please use the form below to contact me.</h3>
-           <!-- <form action="" method="POST">
-                <input name="name" type="text" value="" size="30"/> <br /> Your Name:<br />
-                <input name="number" type="text" value="" size="30"/> <br /> Your Phone Number:<br />
-                <input name="message" type="text" value="" size="30"/> <br /> Your message:<br />
-                <textarea name="message" rows="7" cols="30"> </textarea> <br />
-                <input type="submit" value="Send text"/>
-            </form>-->
             <form>
                 Name: <input type="text" name="name" id="name" value="" size="30" /><br/>
                 Phone Number: <input type="text" name="number" id="number" value="" size="30" /><br/>
